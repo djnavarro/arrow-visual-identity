7df7d2634d459af2506d26387a773069e82464c9
Danielle Navarro <djnavarro@protonmail.com>
2022-02-16 10:38:49 +1100

    updated link generator

--- source/link-generator.R
+++ source/link-generator.R
@@ -8,16 +8,17 @@ val <- 1
 for(orientation in c("horizontal", "vertical", "hex", "chevrons", "text")) {
   for(text in c("white", "black")) {
     for(background in c("white", "black", "transparent")) {
-      
-      fname <- paste0("arrow-logo_", orientation, "_", text, "-txt_", background, "-bg.png")
-      if(file.exists(here::here("img", fname))) {
+      for(format in c("png", "svg")) {
         
-        msg1[[val]] <- paste0("[", add+val, "]: {{ site.baseurl }}/img/", fname)
-        msg2[[val]] <- paste0("- [Arrow logo with ", orientation, " orientation, ", text, " text, and ", background, " background][", add+val, "]")
-        val <- val + 1
-        
+        fname <- paste0("arrow-logo_", orientation, "_", text, "-txt_", background, "-bg.", format)
+        if(file.exists(here::here("img", fname))) {
+          
+          msg1[[val]] <- paste0("[", add+val, "]: {{ site.baseurl }}/img/", fname)
+          msg2[[val]] <- paste0("- [Arrow logo (", toupper(format) ," format) with ", orientation, " orientation, ", text, " text, and ", background, " background][", add+val, "]")
+          val <- val + 1
+          
+        }
       }
-      
     }
   }
 }
